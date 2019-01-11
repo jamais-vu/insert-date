5fdd614a711918f0056fd58ac27f79d0e333c424
jv <46115232+jamais-vu@users.noreply.github.com>
2019-01-11 04:04:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Sublime Text 3 plugin to insert the current local date or time at the location 
+of the first cursor. This will replace any selected text.
+
+Date: "ctrl+alt+shift+d". Date is formatted 'YYYY-MM-DD '.
+Time: "ctrl+alt+shift+t". Time is formatted 'HH:MM '.
+
+At the moment the only default keymap is for Windows.
+
+My first Sublime plugin. I wanted a simple way to insert the time or date when
+I am writing notes. The Package Control website was experiencing an unknown 
+error so I had to write my own. 
+
+The tutorial at https://cnpagency.com/blog/creating-sublime-text-3-plugins-part-1/ helped me get started.
---
+++ insertdate.py
@@ -0,0 +1,16 @@
+import sublime
+import sublime_plugin
+import datetime
+
+# Sublime Text supports multiple cursors in a single file, so we use 
+# self.view.sel()[0].begin() to get the location of the first cursor.
+
+class DateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.replace(edit, self.view.sel()[0].begin(), 
+                         str(datetime.datetime.now().date()) + ' ')
+
+class TimeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.replace(edit, self.view.sel()[0].begin(),
+                         str(datetime.datetime.now().time())[0:5] + ' ')
